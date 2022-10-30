44c8a7c41cdbef377d47f3699f3c95558e92695d
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-10-30 17:04:14 +0100

    feat: remove ! following ayoub

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomePage() {
       <HeaderBanner />
       <main className="h-screen mx-auto max-w-8xl flex items-center justify-center">
         <h1 className="text-4xl leading-snug font-display font-bold text-slate-800 md:text-5xl lg:text-6xl">
-          Fullstack <span className="text-indigo-600">Developer!</span>
+          Fullstack <span className="text-indigo-600">Developer</span>
         </h1>
       </main>
     </>
