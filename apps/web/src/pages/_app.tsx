60e242bf7fd31858eff3bdc2b183196abab6642c
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-10-30 15:23:13 +0100

    feat: init project

---
+++ apps/web/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { FC, PropsWithChildren, ReactNode } from 'react';
+import { AppProps } from 'next/app';
+
+import '@/styles/global.css';
+
+interface AppWithLayoutProps extends AppProps {
+  Component: AppProps['Component'] & {
+    Layout: FC<PropsWithChildren>;
+  };
+}
+
+const Noop = ({ children }: { children: ReactNode }) => children;
+
+export default function MyApp({ Component, pageProps }: AppWithLayoutProps) {
+  const Layout = Component.Layout || Noop;
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <main className='h-screen mx-auto max-w-8xl flex items-center justify-center'>
+      <h1 className='text-4xl leading-snug font-display font-bold text-slate-800 md:text-5xl lg:text-6xl'>
+        Coming <span className='text-green-600'>Soon!</span>
+      </h1>
+    </main>
+  );
+}
+
+export default HomePage;
