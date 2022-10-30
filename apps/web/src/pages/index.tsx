e2be3f12b067feea60a5c693d467c7e241e07c51
soramon0 <23435312+soramon0@users.noreply.github.com>
2022-10-30 15:47:26 +0100

    feat: add page title

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,18 @@
+import Head from 'next/head';
+
 function HomePage() {
   return (
-    <main className='h-screen mx-auto max-w-8xl flex items-center justify-center'>
-      <h1 className='text-4xl leading-snug font-display font-bold text-slate-800 md:text-5xl lg:text-6xl'>
-        Coming <span className='text-green-600'>Soon!</span>
-      </h1>
-    </main>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className='h-screen mx-auto max-w-8xl flex items-center justify-center'>
+        <h1 className='text-4xl leading-snug font-display font-bold text-slate-800 md:text-5xl lg:text-6xl'>
+          Coming <span className='text-green-600'>Soon!</span>
+        </h1>
+      </main>
+    </>
   );
 }
 
