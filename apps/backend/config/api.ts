62c7a87a4c9f74dfaf2312731cb7e79e45ee820c
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-08-21 18:06:03 +0100

    remove strapi backend

--- apps/backend/config/api.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
-    withCount: true,
-  },
-};
-
-export default config;
--- apps/backend/config/server.ts
+++
@@ -1,9 +0,0 @@
-const configure = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
-
-export default configure;
